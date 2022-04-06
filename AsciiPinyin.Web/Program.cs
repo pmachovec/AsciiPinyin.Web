705ceba9c35da5df986b089fd8ece28d388b78e9
Petr Machovec <pmachovec@outlook.com>
2022-04-06 13:54:38 +0200

    Json database service

--- AsciiPinyin.Web/Program.cs
+++ AsciiPinyin.Web/Program.cs
@@ -1,7 +1,10 @@
+using AsciiPinyin.Web.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddTransient<ChacharJsonService>();
 
 var app = builder.Build();
 
---
+++ AsciiPinyin.Web/Services/ChacharJsonService.cs
@@ -0,0 +1,48 @@
+﻿using AsciiPinyin.Web.Models;
+using System.Text.Json;
+
+namespace AsciiPinyin.Web.Services
+{
+    public class ChacharJsonService
+    {
+        public ChacharJsonService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        /*
+         * Provider of information about the web hosting environment an application is running in.
+         * It's another service that we get it automatically from .NET in the constructor (dependency injection).
+         */
+        private IWebHostEnvironment WebHostEnvironment { get; }
+
+        // Path to the database file retrieved programmatically from the WebHostEnvironment service.
+        private string ChacharJsonFilePath
+        {
+            get
+            {
+                return Path.Combine( // Path as string creator
+                    WebHostEnvironment.WebRootPath, // Path to the 'wwwroot' folder
+                    "database",
+                    "chachars.json");
+            }
+        }
+
+        /**
+         * Transfers the Json database file (which must be in the expected location) to an IEnumerable of Chachars.
+         */
+        public IEnumerable<Chachar> GetChachars()
+        {
+            using var fileReader = File.OpenText(ChacharJsonFilePath);
+
+            var chachars = JsonSerializer.Deserialize<Chachar[]>(
+                fileReader.ReadToEnd(),
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+            return chachars ?? Enumerable.Empty<Chachar>();
+        }
+    }
+}
