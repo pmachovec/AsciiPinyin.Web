0923fa3feacea4dde4ccf2d1a0d05c3b64f421ad
Petr Machovec <pmachovec@outlook.com>
2022-05-01 19:36:54 +0200

    Dynamic page title
    - No way without JavaScript.

--- AsciiPinyin.Web/wwwroot/js/site.js
+++ AsciiPinyin.Web/wwwroot/js/site.js
@@ -1,4 +1,2 @@
-// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+// In Blazor Server application, there's no way how to achieve dynamic title changes without JavaScript.
+setTitle = (title) => { document.title = "ASCII Pinyin - " + title; };
