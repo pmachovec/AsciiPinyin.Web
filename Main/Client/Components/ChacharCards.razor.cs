5163fac7056bdba2110b8e6f05de3686746e3a90
Petr Machovec <pmachovec@outlook.com>
2022-05-23 11:51:41 +0200

    Chachar cards to separate component

---
+++ Main/Client/Components/ChacharCards.razor.cs
@@ -0,0 +1,15 @@
+using AsciiPinyin.Web.Shared.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace AsciiPinyin.Web.Client.Components;
+
+public class ChacharCardsBase: ComponentBase
+{
+    #pragma warning disable CS8618
+    [Parameter]
+    public Chachar[] Chachars { protected get; set; }
+
+    [Parameter]
+    public Action<Chachar> SelectChachar { protected get; set; }
+    #pragma warning restore CS8618
+}
