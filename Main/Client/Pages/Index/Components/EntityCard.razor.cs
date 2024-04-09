e8e47f74d8a2004daf6d8278f2a82602fdfb80ee
Petr Machovec <pmachovec@outlook.com>
2024-04-09 17:21:31 +0200

    Entity cards as reusable components

---
+++ Main/Client/Pages/Index/Components/EntityCard.razor.cs
@@ -0,0 +1,19 @@
+using AsciiPinyin.Web.Shared.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace AsciiPinyin.Web.Client.Pages.Index.Components;
+
+public class EntityCardBase<TEntity> : ComponentBase where TEntity : IEntity
+{
+    [Parameter]
+    public char Body { get; set; } = default!;
+
+    [Parameter]
+    public string Footer { get; set; } = default!;
+
+    [Parameter]
+    public Action<TEntity> SelectEntity { get; set; } = default!;
+
+    [Parameter]
+    public TEntity SelectEntityArgument { get; set; } = default!;
+}
