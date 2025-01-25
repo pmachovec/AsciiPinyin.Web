d929589dd4af33b6d6442b4129b3dd5effa712ae
Petr Machovec <pmachovec@outlook.com>
2025-01-25 18:37:12 +0100

    Make report generator to shut up about missing generator files

--- coverage.ps1
+++ coverage.ps1
@@ -5,4 +5,5 @@ rm -Recurse -Force ".\${coverageResults}" -ErrorAction SilentlyContinue;
 rm -Recurse -Force ".\${testResults}" -ErrorAction SilentlyContinue;
 
 dotnet test --collect:"XPlat Code Coverage" --results-directory:"$testResults"
-reportgenerator -reports:".\${testResults}\*\coverage.cobertura.xml" -targetdir:"$coverageResults" -reporttypes:Html
+
+reportgenerator -reports:".\${testResults}\*\coverage.cobertura.xml" -targetdir:"$coverageResults" -filefilters:"-*Microsoft.Extensions.Logging.Generators\Microsoft.Extensions.Logging.Generators.LoggerMessageGenerator\LoggerMessage.g.cs;-*System.Text.RegularExpressions.Generator\System.Text.RegularExpressions.Generator.RegexGenerator\RegexGenerator.g.cs"
